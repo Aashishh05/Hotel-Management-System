bcadaab1947951fba0f233d447108a8d1671c678
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-02 09:11:02 +0545

    added role and permission seed

---
+++ Server/src/seed/rolesSeed.js
@@ -0,0 +1,54 @@
+const roles = [
+  {
+    name: "superadmin",
+    displayName: "Super Admin",
+    isSystem: true,
+  },
+  {
+    name: "hoteladmin",
+    displayName: "Hotel Admin",
+    isSystem: true,
+  },
+  {
+    name: "frontdesk",
+    displayName: "Front Desk",
+    isSystem: false,
+  },
+  {
+    name: "housekeeper",
+    displayName: "Housekeeper",
+    isSystem: false,
+  },
+  {
+    name: "maintenance",
+    displayName: "Maintenance",
+    isSystem: false,
+  },
+  {
+    name: "accountant",
+    displayName: "Accountant",
+    isSystem: false,
+  },
+  {
+    name: "restaurantmanager",
+    displayName: "Restaurant Manager",
+    isSystem: false,
+  },
+  {
+    name: "chef",
+    displayName: "Chef",
+    isSystem: false,
+  },
+  {
+    name: "securitystaff",
+    displayName: "Security Staff",
+    isSystem: false,
+  },
+  {
+    name: "guest",
+    displayName: "Guest",
+    isSystem: false,
+  },
+];
+
+export default roles;
