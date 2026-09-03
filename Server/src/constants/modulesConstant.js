80efab5e675adac588a939e50bc441f3986f8987
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-03 09:05:06 +0545

    added roles and modules constants

---
+++ Server/src/constants/modulesConstant.js
@@ -0,0 +1,22 @@
+const MODULES = [
+  "dashboard",
+  "users",
+  "roles",
+  "permissions",
+  "rooms",
+  "bookings",
+  "checkin-checkout",
+  "housekeeping",
+  "maintenance",
+  "billing",
+  "payments",
+  "restaurant",
+  "menu",
+  "guests",
+  "reports",
+  "audit-logs",
+  "settings",
+  "notifications",
+];
+
+export default MODULES;
---
+++ Server/src/constants/rolesConstant.js
@@ -0,0 +1,15 @@
+const ROLES = {
+  SUPER_ADMIN: "superadmin",
+  HOTEL_ADMIN: "hoteladmin",
+  FRONT_DESK: "fronedesk",
+  HOUSEKEEPER: "housekeeper",
+  MAINTENANCE: "maintenance",
+  ACCOUNTANT: "accountant",
+  RESTAURANT_MANAGER: "restaurantmanager",
+  CHEF: "chef",
+  SECURITY_STAFF: "securitystaff",
+  GUEST: "guest",
+};
+
+
+export default ROLES;
