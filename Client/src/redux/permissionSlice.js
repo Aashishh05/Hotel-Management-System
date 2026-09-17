73780f9350115935cabace38f8a72208fd8a42c4
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-17 09:46:41 +0545

    added permission redux slice

---
+++ Client/src/redux/permissionSlice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { act } from "react";
+
+const initialState = {
+  permissions: {},
+  loading: false,
+  error: null,
+};
+
+const permissionSlice = createSlice({
+  name: "permission",
+  initialState,
+
+  reducers: {
+    setPermission(state, action) {
+      state.permissions = action.payload;
+      state.error = null;
+    },
+
+    clearPermission(state) {
+      ((state.permissions = {}), (state.error = null));
+    },
+
+    setPermissionLoading(state, action) {
+      state.loading = action.payload;
+    },
+
+    setPermissionError(state, action) {
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  setPermission,
+  clearPermission,
+  setPermissionLoading,
+  setPermissionError,
+} = permissionSlice.actions;
+
+export default permissionSlice.reducer;
