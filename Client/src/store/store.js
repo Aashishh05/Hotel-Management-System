1576e14d0a2bd77117e36a103c4eb38fa93299a8
Aashishh05 <shresthaaashish246@gmail.com>
2026-09-17 09:50:18 +0545

    added slices to store

--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,10 +1,10 @@
-
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../redux/authSlice.js"
-
+import authReducer from "../redux/authSlice.js";
+import permissionReducer from "../redux/permissionSlice.js";
 
 export const store = configureStore({
-    reducer:{
-        auth:authReducer
-    }
-})
+  reducer: {
+    auth: authReducer,
+    permission: permissionReducer,
+  },
+});
